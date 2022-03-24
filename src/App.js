4cdba93d3bfcb80868fe5a0d25b5c5e0f068eda8
Rony Derra <derrarony@gmail.com>
2022-03-25 01:15:49 +0200

    react-beautiful-dnd

--- src/App.js
+++ src/App.js
@@ -1,40 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import userdata from "./tempData.json";
+import { useState } from "react";
 
-  
-  var el = document.getElementById('table');
-  var dragger = tableDragger(el, {
-    mode: 'row',
-    dragHandler: '.handle',
-    onlyBody: true,
-    animation: 300
-  });
-  dragger.on('drop', function (from, to) {
-    console(from);
-    console(to);
-  });
+export default function App() {
+  const [users, setUsers] = useState(userdata.data);
 
-
+  const handleDragEnd = (e) => {
+    if (!e.destination) return;
+    let tempData = Array.from(users);
+    let [source_data] = tempData.splice(e.source.index, 1);
+    tempData.splice(e.destination.index, 0, source_data);
+    setUsers(tempData);
+  };
   return (
-    <div className="App">
-      <table id="table">
-        <thead>
-          <tr>
-            <th class='handle'>header1</th>
-            <th class='handle'>header2</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>conten1</td>
-            <td>conten2</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="App mt-4">
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <table className="table borderd">
+          <thead>
+            <tr>
+              <th />
+              <th>Username</th>
+              <th>Age</th>
+              <th>Gender</th>
+            </tr>
+          </thead>
+          <Droppable droppableId="droppable-1">
+            {(provider) => (
+              <tbody
+                className="text-capitalize"
+                ref={provider.innerRef}
+                {...provider.droppableProps}
+              >
+                {users?.map((user, index) => (
+                  <Draggable
+                    key={user.name}
+                    draggableId={user.name}
+                    index={index}
+                  >
+                    {(provider) => (
+                      <tr {...provider.draggableProps} ref={provider.innerRef}>
+                        <td {...provider.dragHandleProps}> = </td>
+                        <td>{user.name}</td>
+                        <td>{user.age}</td>
+                        <td>{user.gender}</td>
+                      </tr>
+                    )}
+                  </Draggable>
+                ))}
+                {provider.placeholder}
+              </tbody>
+            )}
+          </Droppable>
+        </table>
+      </DragDropContext>
     </div>
   );
 }
-
-export default App;
